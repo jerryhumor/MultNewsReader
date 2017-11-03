1eef5c35ccde06070edbd53bd7ef5aaa0d592cf9
jerryhumor <hanjianhao@tpson.cn>
2017-11-03 19:31:00 +0800

    初步创建项目

--- app/src/main/java/com/hdu/jerryhumor/multnewsreader/NewsListActivity.java
+++ app/src/main/java/com/hdu/jerryhumor/multnewsreader/NewsListActivity.java
@@ -1,8 +1,13 @@
 package com.hdu.jerryhumor.multnewsreader;
 
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.RecyclerView;
+
 import com.hdu.jerryhumor.multnewsreader.activity.BaseActivity;
 
-public class NewsListActivity extends BaseActivity {
+public class NewsListActivity extends BaseActivity{
+
+    private TabLayout tabLayout;
 
     @Override
     protected int getResourceId() {
